cf8fa7eaa2854b8ffd474e7b93559c83350f1492
Santygd <santy.gd@gmail.com>
2015-05-24 15:13:13 +0100

    Nuevo link
    Nuevo link a la página de Oceania.
    Eliminar ficheros innecesarios

--- www/img/Mapa de nueva zelanda_files/paygol.js
+++
@@ -1,3 +0,0 @@
-document.write('<script src=https://www.paygol.com/micropayment/js/pg.js type=text/javascript></script>');
-document.write('<link rel=stylesheet href=https://www.paygol.com/micropayment/css/pg.css type=text/css media=screen />');
-
